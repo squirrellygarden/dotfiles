41a3127b2f4ffa2bdc468753e2869ce94ecc32d5
squirrelly garden <git@squirrelly.garden>
2026-04-01 21:10:12 -0400

    Fix certified gtk moment

--- restore_configs.sh
+++ restore_configs.sh
@@ -10,6 +10,8 @@ cp -r .config/ ~/.config/
 
 echo "Restoring gtk theme..."
 cp -r gtk/catppuccin-mocha-mauve-standard ~/.local/share/themes
+cp -r gtk/gtk-3.0 ~/.config
+cp -r gtk/gtk-4.0 ~/config
 
 echo "Restoring krita scheme..."
 cp -r krita/color-schemes/ ~/.local/share/krita
